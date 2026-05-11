976c00f93bc7a99b1f48f1bcaf91c8e8014e16dd
Wang Yiyang <wangyiyang.kk@gmail.com>
2026-05-11 17:14:38 +0800

    fix: suppress eslint warning in test mock

--- apps/web/src/components/song-card-cover.test.tsx
+++ apps/web/src/components/song-card-cover.test.tsx
@@ -9,6 +9,7 @@ vi.mock('next/link', () => ({
 }))
 
 vi.mock('next/image', () => ({
+  // eslint-disable-next-line @next/next/no-img-element
   default: ({ alt, ...props }: { alt: string }) => <img alt={alt} {...props} />,
 }))
 
